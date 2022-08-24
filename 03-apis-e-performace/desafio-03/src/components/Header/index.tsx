d9775d4b6f203a16e13d01a2f374501ac3694817
Everton Oliveira <tonn.oliveira2011@gmail.com>
2022-08-24 20:24:32 -0300

    feat(desafio-03): criado Header

--- 03-apis-e-performace/desafio-03/src/components/Header/index.tsx
+++ 03-apis-e-performace/desafio-03/src/components/Header/index.tsx
@@ -1,9 +1,15 @@
 import { HeaderContainer } from './styles';
 
+import logo from "../../assets/logo.svg"
+import effect01 from "../../assets/effect01.svg"
+import effect02 from "../../assets/effect02.svg"
+
 export function Header() {
   return (
     <HeaderContainer>
-
+      <img src={effect01} alt="" className=''/>
+      <img src={logo} alt="" className='logo' />
+      <img src={effect02} alt="" />
     </HeaderContainer>
   );
 }
--- 03-apis-e-performace/desafio-03/src/components/Header/styles.ts
+++ 03-apis-e-performace/desafio-03/src/components/Header/styles.ts
@@ -1,5 +1,16 @@
 import styled from 'styled-components';
 
 export const HeaderContainer = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  height: 296px;
 
-`
+  background: ${({ theme }) => theme.profile};
+
+  img.logo {
+    width: 148px;
+    height: 98px;
+  }
+`;
