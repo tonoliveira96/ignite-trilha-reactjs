3932079c4f519efe3621d70829f2cbc61325569e
Everton Oliveira <tonn.oliveira2011@gmail.com>
2022-09-23 15:58:11 -0300

    feat(desafio-04): componente de corrosel

--- 04-fundamentos-do-nextjs/desafio-04/src/pages/index.tsx
+++ 04-fundamentos-do-nextjs/desafio-04/src/pages/index.tsx
@@ -1,39 +1,58 @@
-import type { NextPage } from 'next'
+import type { NextPage } from 'next';
 import Image from 'next/image';
 import { HomeContainer, Products } from '../../styles/pages/home';
 
-import camiseta1 from "../assets/1.png"
-import camiseta2 from "../assets/2.png"
-import camiseta3 from "../assets/3.png"
+import { useKeenSlider } from "keen-slider/react";
+
+import camiseta1 from "../assets/1.png";
+import camiseta2 from "../assets/2.png";
+import camiseta3 from "../assets/3.png";
+
+import "keen-slider/keen-slider.min.css";
 
 const Home: NextPage = () => {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48
+    }
+  });
+
   return (
-    <HomeContainer>
-      <Products>
-        <Image src={camiseta1} width={520} height={480} alt=""/>
+    <HomeContainer ref={ sliderRef } className="keen-slider">
+      <Products className="keen-slider__slide">
+        <Image src={ camiseta1 } width={ 520 } height={ 480 } alt="" />
         <footer>
           <strong>Camiseta x</strong>
           <span>R$ 79,90</span>
         </footer>
       </Products>
 
-      <Products>
-        <Image src={camiseta2} width={520} height={480} alt=""/>
+      <Products className="keen-slider__slide">
+        <Image src={ camiseta2 } width={ 520 } height={ 480 } alt="" />
         <footer>
           <strong>Camiseta x</strong>
           <span>R$ 79,90</span>
         </footer>
       </Products>
 
-      <Products>
-        <Image src={camiseta3} width={520} height={480} alt=""/>
+      <Products className="keen-slider__slide">
+        <Image src={ camiseta3 } width={ 520 } height={ 480 } alt="" />
+        <footer>
+          <strong>Camiseta x</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Products>
+
+      <Products className="keen-slider__slide">
+        <Image src={ camiseta3 } width={ 520 } height={ 480 } alt="" />
         <footer>
           <strong>Camiseta x</strong>
           <span>R$ 79,90</span>
         </footer>
       </Products>
     </HomeContainer>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
