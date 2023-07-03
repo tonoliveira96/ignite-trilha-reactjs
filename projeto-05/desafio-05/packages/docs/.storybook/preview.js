3ef16d39a161e0062705a8dcc2efa1e78b8bc360
Everton Oliveira <tonn.oliveira2011@gmail.com>
2023-07-03 20:43:23 -0300

    feat(projeto-05): Documentação de Cores

--- projeto-05/desafio-05/packages/docs/.storybook/preview.js
+++ projeto-05/desafio-05/packages/docs/.storybook/preview.js
@@ -1,3 +1,5 @@
+import { themes } from "@storybook/theming";
+
 /** @type { import('@storybook/react').Preview } */
 const preview = {
   parameters: {
@@ -8,7 +10,11 @@ const preview = {
         date: /Date$/,
       },
     },
+    docs: {
+      theme: themes.dark,
+    }
   },
+
 };
 
 export default preview;
