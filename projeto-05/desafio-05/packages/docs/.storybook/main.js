8e625cbdbdf948d66ce541e98df642965b5f94cb
Everton Oliveira <tonn.oliveira2011@gmail.com>
2023-07-12 21:53:13 -0300

    fix(projeto-05): Muda diretório de workflow

--- projeto-05/desafio-05/packages/docs/.storybook/main.js
+++ projeto-05/desafio-05/packages/docs/.storybook/main.js
@@ -14,5 +14,12 @@ const config = {
   docs: {
     autodocs: "tag",
   },
+  viteFinal: (config, { configType }) => {
+    if (configType === 'PRODUCTION') {
+      config.base = '/projeto-05/'
+    }
+
+    return config
+  }
 };
 export default config;
