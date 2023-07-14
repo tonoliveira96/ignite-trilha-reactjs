dd4c8104c89a2a39cb3f2a1c31ab02b9fe22030b
Everton Oliveira <tonn.oliveira2011@gmail.com>
2023-07-14 08:28:27 -0300

    feat(projeto-05): Melhoria Button, BoX e TextInput

--- projeto-05/desafio-05/packages/docs/.storybook/main.js
+++ projeto-05/desafio-05/packages/docs/.storybook/main.js
@@ -16,7 +16,7 @@ const config = {
   },
   viteFinal: (config, { configType }) => {
     if (configType === 'PRODUCTION') {
-      config.base = '/ignite-trilha-reactjs-2022/'
+      config.base = '/ignite-trilha-reactjs-2022/projeto-05/desafio-05'
     }
 
     return config
