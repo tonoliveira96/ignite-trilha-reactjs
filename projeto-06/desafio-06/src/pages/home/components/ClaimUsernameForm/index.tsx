da15755b877c287c40fd73d419dd46f04ba9b118
Everton Oliveira <tonn.oliveira2011@gmail.com>
2023-10-17 09:40:27 -0300

    feat(projeto-06): react-hook-form

--- projeto-06/desafio-06/src/pages/home/components/ClaimUsernameForm/index.tsx
+++ projeto-06/desafio-06/src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -1,14 +1,54 @@
-import { Button, TextInput } from '@ignite-ui/react'
-import { Form } from './styles'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Button, Text, TextInput } from '@ignite-ui/react'
+import { ArrowRight } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { Form, FormAnnotation } from './styles'
+
+const claimUsernameFormSchema = z.object({
+  username: z
+    .string()
+    .min(3, { message: 'Usuário deve ter no mínimo 3 letras' })
+    .regex(/^([a-z\\\\-]+)$/i, {
+      message: 'Usuário deve conter apenas letras e hiféns',
+    })
+    .transform((username) => username.toLowerCase()),
+})
+
+type ClaimUsernameFormData = z.infer<typeof claimUsernameFormSchema>
 
 export function ClaimUsernameForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ClaimUsernameFormData>({
+    resolver: zodResolver(claimUsernameFormSchema),
+  })
+
+  async function handleClaimUsername(data: ClaimUsernameFormData) {}
+
   return (
-    <Form as="form">
-      <TextInput size="sm" prefix="ignite.com" placeholder="seu-usuario" />
-      <Button size="sm" type="submit">
-        Reservar
-        {/* <ArrowRight /> */}
-      </Button>
-    </Form>
+    <>
+      <Form as="form" onSubmit={handleSubmit(handleClaimUsername)}>
+        <TextInput
+          size="sm"
+          prefix="ignite.com"
+          placeholder="seu-usuario"
+          {...register('username')}
+        />
+        <Button size="sm" type="submit">
+          Reservar
+          <ArrowRight />
+        </Button>
+        <FormAnnotation>
+          <Text size="sm">
+            {errors.username
+              ? errors.username.message
+              : 'Digite o nome de usuário desejado'}
+          </Text>
+        </FormAnnotation>
+      </Form>
+    </>
   )
 }
