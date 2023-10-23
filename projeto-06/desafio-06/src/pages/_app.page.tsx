b13c15ea30c15ee360acfbf5e0f7c43d481848cd
Everton Oliveira <tonn.oliveira2011@gmail.com>
2023-10-23 14:37:26 -0300

    feat(projeto-06): Configurando o Next Auth

--- projeto-06/desafio-06/src/pages/_app.page.tsx
+++ projeto-06/desafio-06/src/pages/_app.page.tsx
@@ -1,9 +1,15 @@
+import { SessionProvider } from 'next-auth/react'
+import { AppProps } from 'next/app'
 import { globalStyles } from '../styles/global'
 
 globalStyles()
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  )
 }
 
 export default MyApp
--- projeto-06/desafio-06/src/pages/register/connect-calendar/index.page.tsx
+++ projeto-06/desafio-06/src/pages/register/connect-calendar/index.page.tsx
@@ -1,6 +1,7 @@
 import { Button, Heading, MultiStep, Text } from '@ignite-ui/react'
 import { ArrowRight } from 'phosphor-react'
 // import { api } from "../../../lib/axios"
+import { signIn } from 'next-auth/react'
 import { Container, Header } from '../styles'
 import { ConnectBox, ConnectItem } from './styles'
 
@@ -24,7 +25,11 @@ export default function Register() {
       <ConnectBox>
         <ConnectItem>
           <Text>Google Calendar</Text>
-          <Button variant="secondary" size="sm">
+          <Button
+            variant="secondary"
+            size="sm"
+            onClick={() => signIn('google')}
+          >
             Conectar
             <ArrowRight />
           </Button>
